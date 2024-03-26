d483451a35c09513517b713e3ad43bd62f8fc78b
Ekta <ekta@Ektas-MacBook-Air.local>
2024-03-26 12:24:45 +0530

    fixed cors issue

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express =  require("express"); 
 const products = require('./db/products');
+const cors = require("cors");
+
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/" , (req, res)=>{
